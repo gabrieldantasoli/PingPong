a7c8c734dc62166875b37c8927a2bc1c7bf27bc2
Gabriel Dantas de Oliveira <gabriel36dantas@gmail.com>
2022-02-06 11:10:04 -0300

    fisrt files ping pong

---
+++ interations/script.js
@@ -0,0 +1,30 @@
+var vbtiniciar ;
+var vbola ;
+var vcpu ;
+var vjogador ;
+var vpaineltxtpoints ;
+
+//animação
+var games , frames ;
+
+//posições
+var posbolax , posbolay ;
+var posjogadorx , posjogadory ;
+var poscpux , poscpuy ;
+
+//direções
+var dirJy ;
+
+//posições iniciais
+var posjoginiy = 50 + '%',poscpuinity = 50 + '%',posbolainitx = 50 + '%',posbolainity = 50 + '%';
+
+//direção
+var bolay,bolax ; 
+var jogy,cpuy ;
+
+//velocidade 
+var vbola,vcpu,vjogador ;
+
+//controle 
+var pontos = 0 ;
+var tecla ;
