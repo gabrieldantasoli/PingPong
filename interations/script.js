58335b52805997ca716b1a70efa32c8988a2f8e9
Gabriel Dantas de Oliveira <gabriel36dantas@gmail.com>
2022-02-07 21:37:43 -0300

    Ping Pong Game

--- interations/script.js
+++ interations/script.js
@@ -21,17 +21,43 @@ var velBol , velPlayer1 , velPlayer2 ;
 var key ;
 var playing = false ;
 
+function controlbol() {
+    posBolX += velBol*bolX ;
+    posBolY += velBol*bolY ;
+
+    //player colision
+    if((posBolX <= 5) && ((posBolY + 5 >= posPlayer1Y - 10) && (posBolY - 5 <= posPlayer1Y + 10))) {
+        bolY = (posBolY-posPlayer1Y)/12
+        bolX *= -1 ; 
+    }else if((posBolX >= 95) && ((posBolY + 5 >= posPlayer2Y - 10) && (posBolY - 5 <= posPlayer2Y + 10))) {
+        bolY = (posBolY-posPlayer2Y)/12
+        bolX *= -1 ; 
+    } ;
+
+    //cpu colision
+
+
+    bol.style.top = posBolY + '%';
+    bol.style.left = posBolX + '%';
+} ;
+start()
+
 function player1Control() {
-    console.log()
     if (playing) {
         posPlayer1Y += velPlayer1*dirPlayer1 ;
+        if (posPlayer1Y >= 90) {
+            posPlayer1Y = 90 ;
+        }else if (posPlayer1Y <= 10) {
+            posPlayer1Y = 10 ;
+        } ;
         player1.style.top = posPlayer1Y + '%';
     } ;
 } ;
 
 function game() {
     if (playing) {
-        player1Control()
+        player1Control() ;
+        controlbol() ;
     } ; 
     frames = requestAnimationFrame(game) ;
 } ; 
@@ -39,13 +65,20 @@ function game() {
 function startGame() {
     if (!playing) {
         cancelAnimationFrame(frames) ;
+        bolY = 0 ;
+        if (Math.random()*10 > 5) {
+            bolX = 1 ;
+        }else{
+            bolX = -1 ;
+        } ;
         playing = true ;
         dirPlayer1 = 0 ;
-        posBolX = defBolX + '%';
-        posBolY = defBolY + '%';
+        posBolX = defBolX ;
+        posBolY = defBolY ;
         posPlayer1Y = defPlayer1Y + '%';
         posPlayer2Y = defPlayer2Y + '%';
-        posPlayer1Y = 50
+        posPlayer1Y = 50 ;
+        posPlayer2Y = 50 ;
         game() ;
     } ;
 } ;
@@ -68,11 +101,10 @@ function start() {
     document.addEventListener('keydown',function(event) {
         key = event.keyCode ;
         if (key == 38) {
-            dirPlayer1 += -1 ;
+            dirPlayer1 = -1 ;
         }else if (key == 40) {
-            dirPlayer1 += 1 ;
+            dirPlayer1 = 1 ;
         }
     }) ; 
 } ;
 
-start() ;
